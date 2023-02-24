f5f2430a89d406c884160d7c145596c1046637b3
Mell <emochka161297@gmail.com>
2023-02-24 21:18:41 +0300

    Task1

---
+++ sem1task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Является ли первое число квадратом второго
+
+Console.Write("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine());
+
+if (numB == numA * numA)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
