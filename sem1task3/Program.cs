aab8c01103a4c1581b8d9d60f76f03974b68b43b
Mell <emochka161297@gmail.com>
2023-02-24 22:02:24 +0300

    Task2

--- sem1task3/Program.cs
+++ sem1task3/Program.cs
@@ -1,1 +1,33 @@
-﻿// 
+﻿// По числу день недели
+
+Console.WriteLine("Введите номер дня недели: ");
+int day = int.Parse(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+         Console.Write("Понедельник");
+         break;
+    case 2:
+         Console.Write("Вторник");
+         break;
+    case 3:
+         Console.Write("Стреда");
+        break;
+    case 4:
+         Console.Write("Четверг");
+        break;
+    case 5:
+         Console.Write("Пятница");
+        break;
+    case 6:
+         Console.Write("Суббота");
+         break;
+    case 7:
+          Console.Write("Вторник");
+          break;
+    default:
+         Console.Write("Вы ввели не день недели");
+         break;
+
+}
