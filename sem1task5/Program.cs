5877901cee18c8d0333d5c27da103673a1043edd
Mell <emochka161297@gmail.com>
2023-02-25 16:25:33 +0300

    dz

---
+++ sem1task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа, которая принимает на вход одно число N, и показывает все целые числа от -N до N.
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int count = -N-1;
+int num = 0;
+
+if (N > 0)
+{
+    while(count < N)
+    {
+        num = count + 1;
+        Console.WriteLine(num);
+        count ++;
+    }
+}
+else
+{
+    Console.Write("Некорректное число");
+}
--- sem1task8/Program.cs
+++ sem1task8/Program.cs
@@ -1,3 +1,22 @@
 ﻿// Принимает на вход число N, а на выходе показывает все чётные числа от 1 до N
 
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int count = 0;
 
+if (N < 0)
+{
+    while(count < N + 1)
+    {
+        count -= 2;
+        Console.WriteLine($"{count}"); 
+    }
+}
+else
+{
+    while (count < N - 1)
+    {
+        count += 2;
+        Console.WriteLine(count);
+    }
+}
