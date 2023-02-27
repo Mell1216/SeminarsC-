7ae748b174333e7aa7af9e93f572b4968ce36d97
Mell <emochka161297@gmail.com>
2023-02-27 21:57:26 +0300

    Task11

---
+++ sem2task11/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+Console.Write("Введите трёзначное число: ");
+int num = new Random().Next(100, 1000);
+int first = num / 100;
+int sec = num % 10;
+
+Console.WriteLine(num);
+Console.Write(first);
+Console.Write(sec);
+
+int newD = first * 10 + sec;
