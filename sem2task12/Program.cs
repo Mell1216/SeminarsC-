f381bdf46fc59744f02c9f816e9703ecedc36825
Mell <emochka161297@gmail.com>
2023-02-28 22:00:43 +0300

    Task16

---
+++ sem2task12/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и выводит, является ли второе число кратно первому.
+// И если не кратно, то выводит остаток
+
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int sum = num1 % num2;
+
+if(sum == 0)
+{
+    Console.WriteLine($"Число кратно {num2}");
+}
+else
+{
+    Console.WriteLine($"Число некратно {num2}");
+    Console.WriteLine($"Остаток от деления {sum}");
+}
---
+++ sem2task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, и проверяет кратно ли оно одновременно двум числам
+
+Console.WriteLine("Введите число: ");
+int sum = int.Parse(Console.ReadLine());
+
+int num1 = new Random().Next(1, 10);
+int num2 = new Random().Next(1, 100);
+
+if(num1 == 0 & num2 == 0)
+{
+    Console.Write($"Число {sum} кратно {num1} и {num2}");
+}
+else
+{
+    Console.WriteLine($"Числро {sum} не кратно ни {num1}, ни {num2}");
+}
