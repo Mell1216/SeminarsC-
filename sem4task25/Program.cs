833771ee923a06dbb19a42738f80c3de9c880857
Mell <emochka161297@gmail.com>
2023-03-15 19:12:16 +0300

    Task21

---
+++ sem4task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и выводит число А в натуральную степень В
+
+double step(double a, double b)
+{
+    double result = Math.Pow(a, b);
+    return result;
+}
+
+System.Console.Write("Введите число A: ");
+double a = double.Parse(Console.ReadLine());
+
+System.Console.Write("Введите число B: ");
+double b = double.Parse(Console.ReadLine());
+
+double number = step(a, b);
+System.Console.WriteLine($"{a} в степени {b} = {number}");
---
+++ sem4task27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+int sumnumbers(int x)
+{
+    int result = 0;
+    int y = 0;
+
+    while (x != 0)
+    {
+        y = x % 10;
+        x = x / 10;
+        result = result + y;
+    }
+
+    return result;
+}
+
+System.Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+int sum = sumnumbers(n);
+System.Console.WriteLine($"Сумма цифр в числе: {sum}");
