d6114eb2ed951ff038cc579f0eb42618c4d958ec
Mell <emochka161297@gmail.com>
2023-04-04 15:30:48 +0300

    28

--- sem1task8/Program.cs
+++ sem1task8/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Принимает на вход число N, а на выходе показывает все чётные числа от 1 до N
 
 Console.WriteLine("Введите число N");
-int N = int.Parse(Console.ReadLine());
+int N = new Random().Next(1, 100);
 int count = 0;
 
 if (N < 0)
