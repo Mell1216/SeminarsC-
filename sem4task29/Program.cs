675ad52f97a0858cb79d81683fe8a3aca4c8cca8
Mell <emochka161297@gmail.com>
2023-04-12 23:30:34 +0300

    Task29

--- sem4task29/Program.cs
+++ sem4task29/Program.cs
@@ -1,14 +1,33 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
 
-Console.Write("Введите Х: ");
-int x = int.Parse(Console.ReadLine());
-int i = ;
-
-for (int i - 1; i <- x; i++)
+int[] GenerateArray(int Length, int Max, int Min)
+{       
+    
+    int[] array = new int [Length]; 
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
 {
-    if (i == x)
-    {
-        Console.Write(i * i);
-    }
-    Console.Write($"i * i + ");
+        array[i] = random.Next(Min, Max + 1);
 }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length ; i++)
+    {
+        //Console.Write("№");
+        //Console.Write($"{i+1}:");
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+Console.WriteLine("Введдите максимальное значение диапазона случаных чисел которые заполнят массив");
+    int max = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введдите минимальное значение диапазона случаных чисел которые заполнят массив");
+    int min = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите колличество элементов массива");
+    int length = int.Parse(Console.ReadLine());
+int[] array = GenerateArray(length,max,min);
+PrintArray(array);
