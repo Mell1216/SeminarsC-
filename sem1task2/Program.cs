af6ac08171ef41c7e96dcdfda7132152a7998acf
Mell <emochka161297@gmail.com>
2023-05-18 00:48:16 +0300

    Task2

--- sem1task2/Program.cs
+++ sem1task2/Program.cs
@@ -1,18 +1,13 @@
 ﻿// Какое число больше
 
-Console.Write("Введите первое число: ");
-int num1 = int.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine());
-int max = 0;
+int num1 = new Random().Next(1, 100);
+int num2 = new Random().Next(1, 100);
 
 if (num1 > num2)
 {
-    max = num1;
-    Console.Write(max);
+    Console.WriteLine($"{num1} > {num2}");
 }
 else
 {
-    max = num2;
-    Console.Write(max);
+     Console.WriteLine($"{num1} < {num2}");
 }
