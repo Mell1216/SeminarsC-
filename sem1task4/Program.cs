e8aee1c29322ec11732a1aa52015366b18a8be70
Mell <emochka161297@gmail.com>
2023-05-18 00:45:22 +0300

    task4

--- sem1task4/Program.cs
+++ sem1task4/Program.cs
@@ -5,25 +5,21 @@ Console.WriteLine("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int num3 = int.Parse(Console.ReadLine());
-int max = 0;
 
 if (num1 > num2 & num1 > num3)
 {
-    max = num1;
-    Console.WriteLine($"MAX = {max}");
+    Console.WriteLine($"MAX = {num1}");
+
 }
 else
 {
-    if (num2 > num3)
+    if (num2 > num1 & num2 > num3) 
     {
-        max = num2;
-        Console.WriteLine($"MAX = {max}");
-    }    
+       Console.WriteLine($"MAX = {num2}");
+    }
+
     else
     {
-        max = num3;
-        Console.WriteLine($"MAX = {max}");
-
-    }
-    
+        Console.WriteLine($"MAX = {num3}");
+    }    
 }
--- sem2task10/Program.cs
+++ sem2task10/Program.cs
@@ -9,3 +9,8 @@ if(num < 1000 & num > 100 );
     int def = sec / 10;
     Console.WriteLine(def);
 }
+
+
+num = new Random().Next(100, 1000);
+int secNum = num / 10 % 10;
+Console.WriteLine($"Вторая цифра ы числе {num} -> {secNum}"); 
