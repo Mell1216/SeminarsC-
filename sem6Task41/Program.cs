bd3c0e9301c1dbb52a5dce03f109a399bc506c89
Mell <emochka161297@gmail.com>
2023-06-23 12:09:46 +0300

    Task47

---
+++ sem6Task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+
+void GetNumberPositive(int numM)
+{
+    string listNumber = " ";
+    int count = 0;
+    for (int i = 0; i <= numM; i++)
+    {
+        int numberUser = Prompt("Введите число: ");
+        listNumber = listNumber + numberUser +", ";  
+
+        if (numberUser > 0)
+        {
+           count ++;
+        }
+
+    }
+    Console.WriteLine($"{listNumber} чисел больше 0 = {count}");
+}
+
+
+int NumberM = Prompt("Введите количество чисел M: ");
+
+GetNumberPositive(NumberM);
