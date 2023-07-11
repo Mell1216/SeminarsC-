11e42cf7d0fd560e75d7fa849b2813a5a6706f12
Mell <emochka161297@gmail.com>
2023-07-11 23:04:59 +0300

    task68

---
+++ sem9task64/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+(int, int) CheckInput(int M, int N)
+{
+    M = Math.Abs(M);
+    N = Math.Abs(N);
+
+    if (N < M)
+    {
+        int temp = M;
+        M = N;
+        N = temp;
+    }
+    return (M, N);
+}
+void GetEvenNumbers(int M, int N)
+{
+
+    if (M == N)
+    {
+        if ((M % 2) == 0)
+        {
+            Console.Write($"{M}");
+        }
+    }
+    else
+    {
+        if ((M % 2) == 0)
+        {
+            Console.Write($"{M}, ");
+        }
+        M++;
+        GetEvenNumbers(M, N);
+    }
+}
+
+int M = Prompt("Введите число M: ");
+int N = Prompt("Введите число N: ");
+
+(M, N) = CheckInput(M, N);
+
+Console.WriteLine($"Все четные числа из промежутка от {M} до {N} => ");
+GetEvenNumbers(M, N);
---
+++ sem9task66/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+(int, int) CheckInput(int M, int N)
+{
+    M = Math.Abs(M);
+    N = Math.Abs(N); 
+    
+    if (N < M)
+    {
+        int temp = M;
+        M = N;
+        N = temp;
+    }
+    return (M, N);
+}
+
+int GetSumMToN(int m, int n)
+{
+    int sum = 0;
+    if (m == n)
+    {
+        return 0;
+    }
+    else
+    {
+        m++;
+        sum = m + GetSumMToN(m,n);
+        return sum;
+    }
+}
+
+int M = Prompt("Введите число M: ");
+int N = Prompt("Введите число N: ");
+
+(M, N) = CheckInput(M, N);
+
+Console.WriteLine($"Сумма чисел от {M} до {N} = {GetSumMToN((M-1),N)}");
